bcfcfbd298d070ba22bea8f246f6aaeff599ba5d
beyounn <beyounn@8848914e-2522-11de-9896-099eabac0e35>
2009-05-20 20:24:31 +0000

    Initial import for Android x86 support based on cupcake

---
+++ android-x86/system/core/logcat/Android.mk
@@ -0,0 +1,25 @@
+# Copyright 2006 The Android Open Source Project
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= logcat.cpp
+
+LOCAL_SHARED_LIBRARIES := liblog
+
+LOCAL_MODULE:= logcat
+
+include $(BUILD_EXECUTABLE)
+
+########################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := event-log-tags
+
+# This will install the file in /system/etc
+#
+LOCAL_MODULE_CLASS := ETC
+
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+
+include $(BUILD_PREBUILT)
